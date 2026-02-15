30722fb3b99a9d041ed94a34f79f03d5c4b8285c
Fenjazbashi <104022414+hxida@users.noreply.github.com>
2026-02-15 20:20:59 +0300

    Update and rename updates.lua to changelogs.lua

---
+++ changelogs.lua
@@ -0,0 +1,22 @@
+local updates = {
+	{"Patches n stuff - v4.2.0", "- Added \"Sound Settings\".", ""},
+	{"sameVersion", "- Fixed UI positioning.", ""},
+	{"sameVersion", "- Fixed \"Hide Other Players\" issue.", ""},
+	{"sameVersion", "- Added some badges back.", ""},
+	{"Minor Update - v4.1.0", "- Added \"Stats\" menu for players.", ""},
+	{"sameVersion", "- Fixed UI dragging issue.", ""},
+	{"sameVersion", "- Fixed \"Hide Other Players\" issue.", ""},
+	{"sameVersion", "- Badges disabled temporarily.", ""},
+	{"sameVersion", "- Volume button added for MusicBox.", ""},
+	{"Minor Update - v4.0.1", "- Added more new setting options.", ""},
+	{"sameVersion", "- Added new badges.", ""},
+	{"sameVersion", "- Increased player zooming distance.", ""},
+	{"sameVersion", "- Removed unneccessary parts from workspace.", ""},
+	{"sameVersion", "- UI is draggable now.", ""},
+	{"Major Update! - v4.0.0", "- Very new \"Settings, Credits and Changelogs\" menu.", ""},
+	{"sameVersion", "- Badges are fixed.", ""},
+	{"sameVersion", "- Bunch of new features inside of the Settings.", ""},
+	{"sameVersion", "- I forgot.", ""},
+}
+
+return updates
